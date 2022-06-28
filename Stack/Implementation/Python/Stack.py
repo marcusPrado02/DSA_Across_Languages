1709213773deabcffb90e7c7969a67c6dd6951a5
marcusPrado02 <silvamarcusprado@gmail.com>
2022-06-28 07:57:01 -0300

    Stack Implementation in Python

---
+++ Stack/Implementation/Python/Stack.py
@@ -0,0 +1,79 @@
+
+class Stack:
+    def __init__ (self):
+        self.l = 0
+        self.elements = list()
+
+    def clear(self):
+        self.elements.clear()
+        
+    def push (self,i):
+        elements.append(i)
+        l += 1
+    
+    def pop (self):
+        l -= 1
+        return elements.pop()
+
+    def isEmpty(self):
+        return self.elements == []
+
+    def size(self):
+        return len(self.elements)
+    
+    def count(self, i):
+        return self.elements.count(i)
+
+    def index(self, i)
+        return self.elements.index(i)
+
+    def peek(self):
+        ans = elements.pop()
+        elments.append(ans)
+        return ans
+
+    def  printPeek(self):
+        print("Peek Element: %d", peek(self) )
+
+    def sum(self):
+        return sum(self.elements)
+
+    def  printSum(self):
+        print("Sum of Elements: %d", sum(self) )
+    
+    def getLargest(self):
+        return max(self.elements)
+
+    def  printLargest(self):
+        print("Lowest Element: %d ", getLargest() )
+
+    def getLowest(self):
+        return mix(self.elements)
+
+    def  printLowest(self):
+        print("Lowest Element: %d ", getLowest() )
+
+    def getAverage(self):
+        return sum(self)/size(self)
+
+    def  printAverage(self):
+        print("Average of Elements: %d  ", getAverage() )
+
+
+
+    
+
+    
+    
+
+
+    
+
+        
+    
+
+           
+        
+
+
+
